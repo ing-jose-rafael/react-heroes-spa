24197c38294155858a4fda10005b19f77eaf639a
josecarreno <ing.jcarreno@gmail.com>
2022-11-20 23:39:44 -0500

    Animaciones en nuestro componente

--- src/heroes/pages/HeroPage.jsx
+++ src/heroes/pages/HeroPage.jsx
@@ -1,10 +1,12 @@
+import { useMemo } from "react";
 import { Navigate, useNavigate, useParams } from "react-router-dom"
 import { getHeroById } from "../helpers";
 
 export const HeroPage = () => {
   const navigate = useNavigate();
   const { id } = useParams();
-  const hero = getHeroById(id);
+  // useMemo memorizar valores y useCallbak funciones
+  const hero = useMemo(() => getHeroById(id),[id]);
 
   const onNavigateBack = () => {
     // (hero.publisher === 'DC Comics')?
@@ -26,7 +28,7 @@ export const HeroPage = () => {
         <img
           src={`/assets/heroes/${id}.jpg`}
           alt={hero.superhero}
-          className="img-thumbnail" />
+          className="img-thumbnail animate__animated animate__fadeInLeft" />
       </div>
       <div className="col-8">
         <h3>{hero.superhero}</h3>
