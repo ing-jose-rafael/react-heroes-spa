afcb2513ecaadc08cabd8e1eab15be729b9e03ce
josecarreno <ing.jcarreno@gmail.com>
2022-11-22 15:11:06 -0500

    Rutas privadas

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -2,15 +2,23 @@ import React from 'react'
 import { Route, Routes } from 'react-router-dom'
 
 import { LoginPage } from '../auth'
-import {  HeroesRouter } from '../heroes'
+import { HeroesRouter } from '../heroes'
+import { PrivateRouter } from './PrivateRouter'
 
 export const AppRouter = () => {
   return (
     <>
-    
+
       <Routes>
-        <Route path='login' element = {<LoginPage/>}/>
-        <Route path='/*' element = {<HeroesRouter/>}/>
+        <Route path='login' element={<LoginPage />} />
+
+        <Route path='/*' element={
+          <PrivateRouter>
+            <HeroesRouter/>
+          </PrivateRouter>
+        } />
+
+        {/* <Route path='/*' element = {<HeroesRouter/>}/> */}
 
       </Routes>
     </>
---
+++ src/router/PrivateRouter.jsx
@@ -0,0 +1,9 @@
+import { useContext } from 'react';
+import { Navigate } from 'react-router-dom';
+import { AuthContext } from '../auth'
+
+export const PrivateRouter = ({children}) => {
+  const { authState:{logged} } = useContext(AuthContext);
+  // const { logged } = authState;
+  return ( logged ) ? children  : <Navigate to='/login'/>
+}
